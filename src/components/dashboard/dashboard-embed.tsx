d34b0bac78871427383e509e22836443b80c92df
Saurabh Kaushik <syncversesolutions@gmail.com>
2025-06-22 14:12:36 +0000

    but still i can see looker link as name

--- src/components/dashboard/dashboard-embed.tsx
+++ src/components/dashboard/dashboard-embed.tsx
@@ -20,7 +20,7 @@ export default function DashboardEmbed({ url }: DashboardEmbedProps) {
             className="h-full w-full border-0"
             allowFullScreen
             sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-forms allow-popups"
-            title="Looker Studio Dashboard"
+            title="Dashboard"
           ></iframe>
         </div>
       </CardContent>

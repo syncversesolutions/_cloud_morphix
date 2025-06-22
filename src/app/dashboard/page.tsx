6719d02ff6b6917a3be6dde74d08d87cf6c6bf88
Saurabh Kaushik <syncversesolutions@gmail.com>
2025-06-22 16:16:01 +0000

    again for testing hide the looker dashboard url authentication and direc

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -23,7 +23,7 @@ export default function DashboardPage() {
   return (
     <div className="container mx-auto p-4 sm:p-6 lg:p-8">
         <h2 className="mb-4 text-3xl font-bold tracking-tight font-headline">Your Dashboard</h2>
-        <DashboardEmbed url={null} />
+        <DashboardEmbed url="https://lookerstudio.google.com/embed/reporting/0b50f824-7910-4559-8316-369599589d31/page/p_hdy66s3k9c" />
     </div>
   );
 }
